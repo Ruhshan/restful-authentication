cf9602e24abb6df173c196a2ac1ec530e60c7ca3
Ruhshan <ruhshan.ahmed@gmail.com>
2018-06-06 12:46:05 +0600

    added tests for user registration

--- myauth/tests.py
+++ myauth/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+import json
+
+from django.contrib.auth.models import User
+
+class AuthTests(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_registerUserPass(self):
+        data = {
+                "username": "abir",
+                "password": "adfa",
+                "email": "abir@a.com",
+                "first_name": "Abir",
+                "last_name": "Ahmed"
+            }
+        response = self.client.post('/api/v1/myauth/register/', json.dumps(data), content_type="application/json")
+        decoded = json.loads(response.content.decode())
+
+        self.assertEqual(decoded["username"], data["username"])
+
+    def test_registerUserNoPassword(self):
+        data = {
+                "username": "abir",
+                "password": "",
+                "email": "abir@a.com",
+                "first_name": "Abir",
+                "last_name": "Ahmed"
+            }
+        response = self.client.post('/api/v1/myauth/register/', json.dumps(data), content_type="application/json")
+        decoded = json.loads(response.content.decode())
+
+        self.assertEqual(decoded['password'], ['This field may not be blank.'])
 
 # Create your tests here.
+
+
